9de35c84d6243b59286a177aee549e5482f10707
zeekands <azizthea96@gmail.com>
2025-08-24 20:15:38 +0700

    Initial Commit

---
+++ MoviesKuModular/MoviesKuModularApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesKuModularApp.swift
+//  MoviesKuModular
+//
+//  Created by zeekands on 24/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MoviesKuModularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoviesKuModularTests/MoviesKuModularTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesKuModularTests.swift
+//  MoviesKuModularTests
+//
+//  Created by zeekands on 24/08/25.
+//
+
+import Testing
+@testable import MoviesKuModular
+
+struct MoviesKuModularTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
